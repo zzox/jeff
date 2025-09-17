b675dc7ab9e39d6ef20bc3c5075de3e9a52e8b2f
zzox <ty@zzox.net>
2025-09-16 22:44:12 -0500

    start of actor states

--- src/game/actors/Actor.hx
+++ src/game/actors/Actor.hx
@@ -1,8 +1,8 @@
 package game.actors;
 
+import core.Types;
 import core.gameobjects.Sprite;
-import core.util.Util.angleFromPoints;
-import core.util.Util.velocityFromAngle;
+import core.util.Util;
 import kha.Assets;
 
 enum ActorType {
@@ -44,12 +44,27 @@ Jeff => {
 final goodTypes = [Jeff, Fly];
 final badTypes = [Diamond];
 
+enum ActorState {
+    PreAttack;
+    Attack;
+    Other;
+}
+
 class Actor extends Sprite {
     var health:Int;
 
     public var type:ActorType;
 
     public var target:Actor;
+
+    public var state:ActorState = Other;
+    public var stateFrames:Int = 0;
+    public var hurt(get, never):Bool;
+    public var hurtFrames:Int = 0;
+
+    public var attackAngle:Null<Float>;
+    public var attackBaseX:Null<Float>;
+    public var attackBaseY:Null<Float>;
 
     public function new (x:Float, y:Float, type:ActorType) {
         super(x, y, Assets.images.actors, 32, 32);
@@ -59,25 +74,64 @@ class Actor extends Sprite {
     override function update (delta:Float) {
         super.update(delta);
 
+        hurtFrames--;
+
         if (type == Jeff) {
             x += 0.5;
             anim.play('jeff-walk');
+        } else if (state == PreAttack) {
+            stateFrames--;
+            if (stateFrames == 0) {
+                state = Attack;
+                if (target != null) {
+                    attackBaseX = x;
+                    attackBaseY = y;
+                    attackAngle = angleToTarget();
+                    stateFrames = 30;
+                } else {
+                    state = Other;
+                }
+            }
+        } else if (state == Attack) {
+            stateFrames--;
+            if (stateFrames == 0) {
+                state = Other;
+            }
+            // final distance = 
+            // // 60 -> 0
+            // // 45 -> 0.5
+            // // 30 -> 1
+            // // 15 -> 0.5
+            // // 0 -> 0
         } else if (target != null) {
-            final vel = velocityFromAngle(
-                angleFromPoints(
-                    target.getMiddleX(),
-                    target.getMiddleY(),
-                    getMiddleX(),
-                    getMiddleY()
-                ),
-                actorData[type].speed
-            );
+            final vel = velocityFromAngle(angleToTarget(), actorData[type].speed);
 
             x += vel.x * delta;
             y += vel.y * delta;
 
+            if (distanceToTarget() < 16.0) {
+                attack();
+            }
+
             anim.play(actorData[type].attackAnim);
+            visible = hurtFrames <= 0 || Math.floor(hurtFrames / 3) % 2 == 1;
         }
     }
-}
 
+    function attack () {
+        state = PreAttack;
+        stateFrames = 30;
+    }
+
+    function get_hurt () {
+        return hurtFrames > 0;
+    }
+
+    function distanceToTarget ():Float {
+        return distanceBetween(target.getMiddleX(), target.getMiddleY(), getMiddleX(), getMiddleY());
+    }
+
+    function angleToTarget ():Float {
+        return angleFromPoints(target.getMiddleX(), target.getMiddleY(), getMiddleX(), getMiddleY());
+    }
+}
